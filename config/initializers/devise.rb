4a4b378c10a72fa05f079fee20d1bc75b268f7ba
ShobhitWK <shobhit@webkorps.com>
2022-08-20 20:19:37 +0530

    updated devise mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,12 +24,12 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'studentnoticeapi@gmail.com'
 
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
   end
-  
+
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
